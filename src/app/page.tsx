3f748679167d035c66d33ac258031e7060a098be
Mdjuealhossain <mdjueal920977@gmail.com>
2023-07-31 15:40:01 +0600

    feat: create LohIn Template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,24 @@
-
-"use client"
-import "./globals.css"
-import ButtonCom from "./MuiStuleComponents/ButtonComponents/index"
-import AddCard from "./MuiStuleComponents/CardComponents/Addcard"
-import Mui from "./MuiStuleComponents/OverrideMuiComponent/index"
-import SimpoleCard from "./OverRideThemeFoeCard/simpoleCard/index"
-import Typography from "./MuiStuleComponents/Typography/index"
-import {theme} from "./ThemingContainet/createTheme"
-import ComplexInterCard from "./OverRideThemeFoeCard/ComplexIntCard/index"
-import Inputfild from "./OverRideThemeFoeCard/Input/index"
-import BtnGroup from "./MuiStuleComponents/ButtonGroup/index"
-import Cheekbox from "./MuiStuleComponents/CheeckBox/index"
-import FlButton from "./MuiStuleComponents/FAB/index"
-import ListMui from "./MuiStuleComponents/List/index"
-
-
+"use client";
+import "./globals.css";
+import ButtonCom from "./MuiStuleComponents/ButtonComponents/index";
+import AddCard from "./MuiStuleComponents/CardComponents/Addcard";
+import Mui from "./MuiStuleComponents/OverrideMuiComponent/index";
+import SimpoleCard from "./OverRideThemeFoeCard/simpoleCard/index";
+import Typography from "./MuiStuleComponents/Typography/index";
+import { theme } from "./ThemingContainet/createTheme";
+import ComplexInterCard from "./OverRideThemeFoeCard/ComplexIntCard/index";
+import Inputfild from "./OverRideThemeFoeCard/Input/index";
+import BtnGroup from "./MuiStuleComponents/ButtonGroup/index";
+import Cheekbox from "./MuiStuleComponents/CheeckBox/index";
+import FlButton from "./MuiStuleComponents/FAB/index";
+import ListMui from "./MuiStuleComponents/List/index";
+import { Box } from "@mui/material";
+import SignIn from "./TemplateMui/SignIn";
 
 export default function Home() {
   return (
-<div style={{display:"flex",justifyContent:"center" }}>
-<ListMui />
-
-</div>
-  )
+    <Box>
+      <SignIn></SignIn>
+    </Box>
+  );
 }
